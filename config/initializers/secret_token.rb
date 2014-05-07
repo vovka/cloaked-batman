45848626910a1cca3568bf5ce610494f50a38a49
Vovka <sch.icq@gmail.com>
2014-05-07 09:48:19 +0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CloakedBatman::Application.config.secret_key_base = 'd6e7ef5f6cf7841ad3d2cfb0d7692f1f1f4fcd680a94e3be5f5bb0927f93ac138775f6839e24ff680fd3de621815070314577d39b9691fc45018ba62edf60f5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CloakedBatman::Application.config.session_store :cookie_store, key: '_cloaked-batman_session'
