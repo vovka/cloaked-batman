8a8a447ba9f5843113ce5c9b47aaf2f182cd81a5
Volodymyr Shcherbyna <sch.icq@gmail.com>
2014-06-01 17:40:11 +0300

    Products and sheapes in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+#Shapes
+Shape.create(shape_type: "polygon", coordinates: "chslH{rpcD?eDd@yAXdF", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, ipsum aspernatur officia suscipit rem consectetur.")
+Shape.create(shape_type: "rectangle", coordinates: "_eslHu}ocDsCsH", description: "Some shape description")
+Shape.create(shape_type: "rectangle", coordinates: "q|rlHuhpcDgEoF", description: "This is polygon and some one else...")
+
+#Products
+Product.create(name: "Razer Abyssus Gaming Mouse", price: 434.0,side_description: "Мышь Razer Abyssus предназначена для геймеров, кот...",description: "Мышь Razer Abyssus предназначена для геймеров", image: "razer-abyssus-03.jpg")
+Product.create(name: "Razer Kraken Pro", price: 1078.0,side_description: "Kraken Pro обеспечивает сочный и насыщенный звук",description: "Kraken Pro обеспечивает сочный и насыщенный звук в", image: "razerkrakenpro01.jpg")
