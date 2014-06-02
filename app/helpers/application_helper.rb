3587e91baebac34ed86cd692a0b6df7ecc89f0ce
severynsky <severynr@gmail.com>
2014-06-02 16:18:33 +0300

    on progress main, product, store's pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def put_active_class(id_li)
+    params[:controller] == id_li ? 'class="active"' : 'class=""'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ CloakedBatman::Application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :products 
+  resources :products
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
