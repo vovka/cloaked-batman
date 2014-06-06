6c293619577a744cdf2f1b6c4cc38847eaaca088
severynsky <severynr@gmail.com>
2014-06-06 19:42:17 +0300

    addded resources: stores to routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ CloakedBatman::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :products
+  resources :stores
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
