b78e3739ab96fd0e96b031fd169c326a2b559707
severynsky <severynr@gmail.com>
2014-06-07 17:20:26 +0300

    adding validaition -on store creating

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,10 @@
 class Store < ActiveRecord::Base
+
+  # attr_accessible :name, :description, :adress, :telephone, :mail, :contact_person, :map_id
+
+  validates :name, :description, :adress, :telephone, :mail, :contact_person, :map_id, :presence => { 
+     :message =>  "%{value} shouldn't be empty" }
+
+   validates :name, :telephone, :mail, :map_id, :contact_person,  uniqueness: true
+
 end
