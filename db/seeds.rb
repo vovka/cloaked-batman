a6fb299f5059a2a889b81686605ec1b760253cb8
severynsky <severynr@gmail.com>
2014-06-10 10:33:27 +0300

    relations fixing

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,11 +34,3 @@ Shape.create(shape_type: "polygon", coordinates: "chslH{rpcD?eDd@yAXdF", descrip
 Shape.create(shape_type: "rectangle", coordinates: "_eslHu}ocDsCsH", description: "Some shape description")
 Shape.create(shape_type: "rectangle", coordinates: "q|rlHuhpcDgEoF", description: "This is polygon and some one else...")
 
-#Products
-Product.create(name: "Razer Abyssus Gaming Mouse", price: 434.0,side_description: "Мышь Razer Abyssus предназначена для геймеров, кот...",description: "Мышь Razer Abyssus предназначена для геймеров", image: "razer-abyssus-03.jpg")
-Product.create(name: "Razer Kraken Pro", price: 1078.0,side_description: "Kraken Pro обеспечивает сочный и насыщенный звук",description: "Kraken Pro обеспечивает сочный и насыщенный звук в", image: "razerkrakenpro01.jpg")
-
-#Stores
-Store.create(name: "Orlando Shop", description: "Supermarket Orlando Shop", adress: "Lviv, Paternaka,5", telephone: "+380979379992", mail: "mail@mail.com", contact_person: "Oleksiy Pastukhov", map_id: 1)
-Store.create(name: "Bruklin Shop", description: "Supermarket Bruklin Shop", adress: "Lviv, Fedkovycha,20", telephone: "+380979379992", mail: "mail@mail.com", contact_person: "Taras Shyika", map_id: 2)
-Store.create(name: "Texas Shop", description: "Supermarket Texas Shop", adress: "Lviv, V.Velykogo,63", telephone: "+380979379992", mail: "mail@mail.com", contact_person: "Severyn Rybchynsky", map_id: 3)
